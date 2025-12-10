6dc2e6977b201a935f558d002ea4078e22c9fac9
melek saidani <mbcmelek3@gmail.com>
2025-12-10 01:37:39 +0100

    Modify inheritance in twrp_a04.mk
    Updated product inheritance to include mt6765 common configuration.

--- twrp_a04.mk
+++ twrp_a04.mk
@@ -6,12 +6,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Inherit from TWRP common configuration
-$(call inherit-product, vendor/twrp/config/common.mk)
+# Inherit from mt6765 common
+$(call inherit-product, device/samsung/jdm-mt6765-cmn/common.mk)
 
 # SHRP settings
 $(call inherit-product-if-exists, device/samsung/a04/shrp_a04.mk)
